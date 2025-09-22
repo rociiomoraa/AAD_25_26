37133e1445bc4234591cd189cc4bad7f3a0adcfa
rociiomoraa <rocio.mora.garcia02@gmail.com>
2025-09-22 13:22:31 +0200

    Update gitignore

---
+++ src/test/java/com/rocio/aad/AadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocio.aad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

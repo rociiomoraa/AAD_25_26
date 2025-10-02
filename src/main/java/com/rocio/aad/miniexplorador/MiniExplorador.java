bb6b087af6532ea6c419f7d31175446f71e41c27
rociiomoraa <rocio.mora.garcia02@gmail.com>
2025-10-02 13:25:52 +0200

    Paso 2: crear paquete miniexplorador y clase base MiniExplorador

--- src/main/java/com/rocio/aad/miniexplorador/MiniExplorador.java
+++ src/main/java/com/rocio/aad/miniexplorador/MiniExplorador.java
@@ -2,6 +2,6 @@ package com.rocio.aad.miniexplorador;
 
 public class MiniExplorador {
     public static void main(String[] args) {
-        System.out.println("Mini Explorador creado");
+        System.out.println("Mini Explorador creado.");
     }
 }

4dfbc16ddec5f91d7fe8d9e1eaa5ab116685d42d
rociiomoraa <rocio.mora.garcia02@gmail.com>
2025-10-16 12:26:31 +0200

    revert

--- src/main/java/com/rocio/aad/AadApplication.java
+++ src/main/java/com/rocio/aad/AadApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
 
 @SpringBootApplication
 @Slf4j

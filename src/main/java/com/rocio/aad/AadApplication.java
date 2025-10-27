56a9c2ebdefb336dc3830b3d5f13c1d9e47bcc3a
rociiomoraa <rocio.mora.garcia02@gmail.com>
2025-10-27 13:34:58 +0100

    update

--- src/main/java/com/rocio/aad/AadApplication.java
+++ src/main/java/com/rocio/aad/AadApplication.java
@@ -5,17 +5,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 @Slf4j
 public class AadApplication implements CommandLineRunner {
+    private final DataSource dataSource;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AadApplication.class, args);
-	}
+    public AadApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(AadApplication.class, args);
+    }
 
-	}
+    @Override
+    public void run(String... args) {
+        System.out.println("Testing JDBC connection...");
+        try (Connection conn = dataSource.getConnection()) {
+            System.out.println("Connection successful: " + conn.getMetaData().getURL());
+            System.out.println("Database: " + conn.getMetaData().getDatabaseProductName());
+        } catch (Exception e) {
+            System.err.println("Connection failed: " + e.getMessage());
+        }
+    }
 }
